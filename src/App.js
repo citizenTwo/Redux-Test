df2ab10222e2f9f47e840e3d1ff955dd671c3489
Lakshay Raj <rajlakshay13@gmail.com>
2018-06-01 12:45:52 +0530

    Redux setup completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import Post from './components/Posts';
 import PostForm from './components/Postform';
+
+import {Provider} from 'react-redux';
+import store from './store'
 
 class App extends Component {
   render() {
     return (
-      
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
 
-        <PostForm />
-        <hr/ >
-        <Post/>
+          <PostForm />
+          <hr/ >
+          <Post/>
 
-      </div>
+        </div>
+      </Provider>
     );
   }
 }
